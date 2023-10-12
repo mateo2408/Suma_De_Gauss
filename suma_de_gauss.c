63ca28a62fb10f5ae895d5e473072b1de5ed755f
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-12 16:12:54 -0500

    Declaracion de variables

--- suma_de_gauss.c
+++ suma_de_gauss.c
@@ -1,3 +1,11 @@
 /*Construir un programa en lenguaje C que calcule la suma de Gauss a 
 partir de un número entero ingresado por el usuario.*/
 //Mateo Cisneros
+#include<stdio.h>
+main()
+{
+    int num;
+    int sum = 0;
+    int i;
+    return 0;
+}
